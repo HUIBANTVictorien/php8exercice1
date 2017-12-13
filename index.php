a9cd1b7c8f16dbcd672925108b41fb9b6113f1d8
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 09:44:01 +0100

    ajout exo1

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-$number = 0;
-while ($number <= 10) {
-  echo $number.'<br/>';
-  $number ++;
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 1 partie 7 php</title>
+  </head>
+  <body>
+    <form class="form" action="user.php" method="get">
+      <input type="lastname" name="lastname" placeholder="Nom">
+      <input type="firstname" name="firstname" placeholder="Prénom">
+      <button type="submit" name="validate">Valider</button>
+    </form>
+    <?php
+
+    ?>
+  </body>
+</html>
