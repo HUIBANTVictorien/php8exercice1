b9f772956285a79d3d3be81ae917d43c7a6080c8
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 11:39:10 +0100

    ajout exo1

--- index.php
+++ index.php
@@ -2,16 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>exercice 1 partie 7 php</title>
+    <title>exercice 1 partie 8 php</title>
   </head>
   <body>
-    <form class="form" action="user.php" method="get">
-      <input type="lastname" name="lastname" placeholder="Nom">
-      <input type="firstname" name="firstname" placeholder="Prénom">
-      <button type="submit" name="validate">Valider</button>
-    </form>
     <?php
-
+    echo('Le user agent est : ' . $_SERVER['HTTP_USER_AGENT'] . '. L\'adresse ip est : ' . $_SERVER['REMOTE_ADDR'] . '. Le nom du serveur est : ' . $_SERVER['SERVER_NAME']);
     ?>
   </body>
 </html>
